a13c712b542fb6f05cfc68e2c40c0cbf319ce2c0
zhuibobo <j.d.r565>
2019-07-29 20:52:50 +0800

    创建工程

--- jb4dc-builder-web-server/src/main/java/com/jb4dc/builder/webserver/ApplicationBuilderWebServer.java
+++ jb4dc-builder-web-server/src/main/java/com/jb4dc/builder/webserver/ApplicationBuilderWebServer.java
@@ -2,7 +2,9 @@ package com.jb4dc.builder.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -12,9 +14,10 @@ import org.springframework.context.annotation.ComponentScan;
  * To change this template use File | Settings | File Templates.
  */
 
-@SpringBootApplication
-@ComponentScan("com.jb4dc")
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableFeignClients("com.jb4dc")
+@ComponentScan("com.jb4dc")
 public class ApplicationBuilderWebServer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationBuilderWebServer.class, args);
