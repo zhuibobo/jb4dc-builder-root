537ce3a0e5249ab121f7fdb8dca241257e537e3d
zhuibobo <j.d.r565>
2019-08-20 09:07:46 +0800

    创建工程

--- jb4dc-builder-web-package/src/main/front-end/static/Js/HTMLDesignRuntime/Control/WebListControl/WLDCT_FormButton.js
+++ jb4dc-builder-web-package/src/main/front-end/static/Js/HTMLDesignRuntime/Control/WebListControl/WLDCT_FormButton.js
@@ -19,6 +19,38 @@ var WLDCT_FormButton= {
     ClickEvent:function (sender) {
         var $button=sender.data.buttonElem;
         console.log($button);
-
+        var bindauthority=$button.attr("bindauthority");
+        var buttoncaption=$button.attr("buttoncaption");
+        var buttontype=$button.attr("buttontype");
+        var custclientclickbeforemethod=$button.attr("custclientclickbeforemethod");
+        var custclientclickbeforemethodpara=$button.attr("custclientclickbeforemethodpara");
+        var custclientrendereraftermethodpara=$button.attr("custclientrendereraftermethodpara");
+        var custclientrendereraftermethodparapara=$button.attr("custclientrendereraftermethodparapara");
+        var custclientrenderermethod=$button.attr("custclientrenderermethod");
+        var custclientrenderermethodpara=$button.attr("custclientrenderermethodpara");
+        var custserverresolvemethod=$button.attr("custserverresolvemethod");
+        var custserverresolvemethodpara=$button.attr("custserverresolvemethodpara");
+        var formcode=$button.attr("formcode");
+        var formid=$button.attr("formid");
+        var formmoduleid=$button.attr("formmoduleid");
+        var formmodulename=$button.attr("formmodulename");
+        var formname=$button.attr("formname");
+        var id=$button.attr("id");
+        var innerbuttonjsonstring=$button.attr("innerbuttonjsonstring");
+        var opentype=$button.attr("opentype");
+        var operation=$button.attr("operation");
+        var singlename=$button.attr("singlename");
+        var windowcaption=$button.attr("windowcaption");
+        var windowheight=$button.attr("windowheight");
+        var windowwidth=$button.attr("windowwidth");
+        var client_resolve=$button.attr("client_resolve");
+        //debugger;
+        DialogUtility.Frame_OpenIframeWindow(window,DialogUtility.DialogId,BaseUtility.BuildView("/HTML/Builder/Runtime/WebFormRuntime.html",{
+            formId:formid
+        }), {
+            width: windowwidth,
+            height: windowheight,
+            title:windowcaption
+        },1,true);
     }
 }
