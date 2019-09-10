46c5c5630ecec959f8936dc10b0fb0ee8962df40
zhuibobo <j.d.r565>
2019-09-10 20:57:47 +0800

    创建工程

--- jb4dc-builder-web-package/src/main/front-end/static/Js/EditTable/Renderers/TableDesign/EditTable_SelectDefaultValue.js
+++ jb4dc-builder-web-package/src/main/front-end/static/Js/EditTable/Renderers/TableDesign/EditTable_SelectDefaultValue.js
@@ -49,7 +49,7 @@ var EditTable_SelectDefaultValue={
         $inputTxt.attr("fieldDefaultType",fieldDefaultType);
         $inputTxt.attr("fieldDefaultValue",fieldDefaultValue);
         $inputTxt.attr("fieldDefaultText",fieldDefaultText);
-        $inputTxt.val(JBuild4DSelectView.SelectEnvVariable.formatText(fieldDefaultType,fieldDefaultText));
+        $inputTxt.val(DefaultValueUtility.formatText(fieldDefaultType,fieldDefaultText));
         var $inputBtn = $("<input class='normalbutton-v1' style='margin-left: 4px;' type='button' value='...'/>")
         $elem.append($inputTxt).append($inputBtn);
         //将inputtext对象附加到window上,提供给后续的设置值的方法.
@@ -77,7 +77,7 @@ var EditTable_SelectDefaultValue={
             var defaultValue = $inputTxt.attr("fieldDefaultValue");
             var defaultText = $inputTxt.attr("fieldDefaultText");
             var  $elem = $("<div></div>");//一次只能返回一个标签对象
-            $elem.append("<label>" + JBuild4DSelectView.SelectEnvVariable.formatText(defaultType,defaultText) + "</label>");
+            $elem.append("<label>" + DefaultValueUtility.formatText(defaultType,defaultText) + "</label>");
             $elem.append("<label IsSerialize='true' BindName='fieldDefaultType' Value='"+defaultType+"' style='display:none'/>");
             $elem.append("<label IsSerialize='true' BindName='fieldDefaultText' Value='"+defaultText+"' style='display:none'/>");
             $elem.append("<label IsSerialize='true' BindName='fieldDefaultValue' Value='"+defaultValue+"' style='display:none'/>");
@@ -95,7 +95,7 @@ var EditTable_SelectDefaultValue={
             $inputTxt.attr("fieldDefaultType",defaultData.Type);
             $inputTxt.attr("fieldDefaultValue",defaultData.Value);
             $inputTxt.attr("fieldDefaultText",defaultData.Text);
-            $inputTxt.val(JBuild4DSelectView.SelectEnvVariable.formatText(defaultData.Type,defaultData.Text));
+            $inputTxt.val(DefaultValueUtility.formatText(defaultData.Type,defaultData.Text));
         }
         else{
             $inputTxt.attr("fieldDefaultType","");
--- jb4dc-builder-web-package/src/main/front-end/static/Js/VueComponent/Dialog/select-default-value-dialog.js
+++ jb4dc-builder-web-package/src/main/front-end/static/Js/VueComponent/Dialog/select-default-value-dialog.js
@@ -293,3 +293,18 @@ Vue.component("select-default-value-dialog", {
                     </div>
                 </div>`
 });
+var DefaultValueUtility={
+    formatText:function (type,text) {
+        //debugger;
+        if(type=="Const"){
+            return "静态值:【"+text+"】";
+        }
+        else if(type=="EnvVar"){
+            return "环境变量:【"+text+"】";
+        }
+        else if (type == "") {
+            return "【无】";
+        }
+        return "未知类型"+text;
+    }
+}
