a4d9c04fa9838d1ea8d87875137d79dd651dd2a7
zhuibobo <j.d.r565>
2019-11-11 12:13:25 +0800

    创建工程

--- jb4dc-builder-web-package/src/main/front-end/static/Js/HTMLDesignRuntime/Control/WebFormControl/WFDCT_TextDateTime.js
+++ jb4dc-builder-web-package/src/main/front-end/static/Js/HTMLDesignRuntime/Control/WebFormControl/WFDCT_TextDateTime.js
@@ -1,10 +1,22 @@
-var WFDCT_TextDateTime={
-    RendererChain:function (_rendererChainParas) {
+var WFDCT_TextDateTime= {
+    RendererChain: function (_rendererChainParas) {
         //debugger;
         //var $singleControlElem=_rendererChainParas.$singleControlElem;
         //$singleControlElem.val("22222");
     },
-    RendererDataChain:HTMLControl.RendererDataChain,
-    GetValue:HTMLControl.GetValue,
-    SetValue:HTMLControl.SetValue
+    RendererDataChain: HTMLControl.RendererDataChain,
+    GetValue: HTMLControl.GetValue,
+    SetValue: function ($elem, fieldPO, relationFormRecordComplexPo, _rendererDataChainParas) {
+        //debugger;
+        //alert($elem.attr("datetimeformat"));
+        if (fieldPO) {
+            var ctValue=fieldPO.value;
+            var ctDate=DateUtility.ConvertFromString(ctValue);
+            //console.log(fieldPO);
+            ctValue=DateUtility.Format(ctDate,$elem.attr("datetimeformat"));
+            $elem.val(ctValue);
+            $elem.attr("control_value", ctValue);
+            $elem.attr("control_time_value", fieldPO.value);
+        }
+    }
 }
