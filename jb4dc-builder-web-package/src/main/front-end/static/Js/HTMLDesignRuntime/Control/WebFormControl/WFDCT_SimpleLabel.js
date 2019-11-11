306d70c70eb7b3b247583dcd78a389f8b574623e
zhuibobo <j.d.r565>
2019-11-11 12:39:38 +0800

    创建工程

--- jb4dc-builder-web-package/src/main/front-end/static/Js/HTMLDesignRuntime/Control/WebFormControl/WFDCT_SimpleLabel.js
+++ jb4dc-builder-web-package/src/main/front-end/static/Js/HTMLDesignRuntime/Control/WebFormControl/WFDCT_SimpleLabel.js
@@ -4,10 +4,25 @@ var WFDCT_SimpleLabel={
     GetValue:HTMLControl.GetValue,
     SetValue:function ($elem,fieldPO,relationFormRecordComplexPo,_rendererDataChainParas) {
         //debugger;
-        if(fieldPO){
+        if(fieldPO) {
             //console.log(fieldPO);
-            $elem.text(fieldPO.value);
-            $elem.attr("control_value",fieldPO.value);
+            var defformat = $elem.attr("defformat");
+            if(defformat=="yyyy-MM-dd"){
+                var ctValue=fieldPO.value;
+                if(ctValue) {
+                    var ctDate = DateUtility.ConvertFromString(ctValue);
+                    //console.log(fieldPO);
+                    ctValue = DateUtility.Format(ctDate, defformat);
+                }
+                $elem.text(ctValue);
+                $elem.attr("control_value", ctValue);
+                $elem.attr("control_source_value", fieldPO.value);
+            }
+            else{
+                $elem.text(fieldPO.value);
+                $elem.attr("control_value", fieldPO.value);
+                $elem.attr("control_source_value", fieldPO.value);
+            }
         }
     }
 }
