c9e73235f7cfb19f2dc50d8d035163d3ce19b93a
zhuibobo <j.d.r565>
2019-11-29 15:10:27 +0800

    创建工程

--- jb4dc-workflow-web-package/src/main/jb4dc-workflow-modeler/Modeler/webpack.config.js
+++ jb4dc-workflow-web-package/src/main/jb4dc-workflow-modeler/Modeler/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         // 把所有依赖的模块合并输出到一个 bundle.js 文件
         filename: '[name].[hash].js',
         // 输出文件都放到 dist 目录下
-        path: path.resolve(__dirname, './Dist'),
+        path: path.resolve(__dirname, './ModelerDist'),
     },
     mode: 'development',
     module: {
