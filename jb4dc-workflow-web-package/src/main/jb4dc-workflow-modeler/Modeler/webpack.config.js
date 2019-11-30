52e2e4dc2a8ce3975f66151f25fed520e5f889a3
zhuibobo <j.d.r565>
2019-11-30 21:54:05 +0800

    创建工程

--- jb4dc-workflow-web-package/src/main/jb4dc-workflow-modeler/Modeler/webpack.config.js
+++ jb4dc-workflow-web-package/src/main/jb4dc-workflow-modeler/Modeler/webpack.config.js
@@ -21,6 +21,16 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
                 test: /\.less$/,
                 use: [{
                         loader: MiniCssExtractPlugin.loader,
