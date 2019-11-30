cf6cce35f7642e612edde573e4a9e64f27a25fbe
zhuibobo <j.d.r565>
2019-11-30 20:50:56 +0800

    创建工程

--- jb4dc-workflow-web-package/src/main/jb4dc-workflow-modeler/Modeler/VueComponent/flow-bpmnjs-container.vue
+++ jb4dc-workflow-web-package/src/main/jb4dc-workflow-modeler/Modeler/VueComponent/flow-bpmnjs-container.vue
@@ -1,12 +1,31 @@
 <template>
-    <div id="modeler-bpmn-outer">
-        <div class="canvas" id="flow-canvas" style="width: 100%;height: 100%">66666</div>
+    <div id="modeler-bpmn-outer" :height="height" style="height: 800px">
+        <div class="canvas" id="flow-canvas" style="width: 100%;height: 100%"></div>
     </div>
 </template>
 
 <script>
+
+    import BpmnModeler from 'bpmn-js/lib/Modeler';
+    import diagramXML from '../Resources/newDiagram.bpmn';
+    $("#modeler-bpmn-outer").height(PageStyleUtility.GetWindowHeight()-85);
+
     export default {
-        name: "flow-bpmnjs-container"
+        name: "flow-bpmnjs-container",
+        data:function () {
+            return {
+                height:PageStyleUtility.GetWindowHeight()-85
+            }
+        },
+        mounted:function(){
+            var modeler = new BpmnModeler({
+                "container": $('#flow-canvas')[0]
+            });
+            console.log(diagramXML);
+            modeler.importXML(diagramXML, function (err) {
+                console.log(err);
+            });
+        }
     }
 </script>
 
