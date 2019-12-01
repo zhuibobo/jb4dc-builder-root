adedeb438f565a65297fa402dd371fc27d409f67
zhuibobo <j.d.r565>
2019-12-01 11:12:56 +0800

    创建工程

--- jb4dc-workflow-web-package/src/main/jb4dc-workflow-modeler/Modeler/VueComponent/BpmnJsExtend/FlowBpmnJsExtendContainer.js
+++ jb4dc-workflow-web-package/src/main/jb4dc-workflow-modeler/Modeler/VueComponent/BpmnJsExtend/FlowBpmnJsExtendContainer.js
@@ -1,6 +1,15 @@
 import BpmnModeler from 'bpmn-js/lib/Modeler';
 import diagramXML from '../../Resources/newDiagram.bpmn';
+import CustomTranslate from './CustomTranslate';
 let modeler=null;
+
+// Our custom translation module
+// We need to use the array syntax that is used by bpmn-js internally
+// 'value' tells bmpn-js to use the function instead of trying to instanciate it
+var customTranslateModule = {
+    translate: [ 'value', CustomTranslate ]
+};
+
 class FlowBpmnJsExtendContainer {
     defaultSetting = {
         RendererToElemId:""
@@ -13,7 +22,10 @@ class FlowBpmnJsExtendContainer {
         //debugger;
         exConfig = $.extend(true, {}, this.defaultSetting, exConfig);
         modeler = new BpmnModeler({
-            "container": $("#"+exConfig.RendererToElemId)[0]
+            "container": $("#"+exConfig.RendererToElemId)[0],
+            "additionalModules": [
+                customTranslateModule
+            ]
         });
         modeler.importXML(diagramXML, function (err) {
             console.log(err);
