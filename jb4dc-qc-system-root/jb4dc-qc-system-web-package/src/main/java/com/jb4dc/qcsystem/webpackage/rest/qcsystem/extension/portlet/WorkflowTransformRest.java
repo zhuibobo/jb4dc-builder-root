281103cd1f9a4bc46a6daef6b6ce4947dad5115a
zhuibobo <zhuibobo@qq.com>
2021-11-20 16:40:33 +0800

    查询统计

---
+++ jb4dc-qc-system-root/jb4dc-qc-system-web-package/src/main/java/com/jb4dc/qcsystem/webpackage/rest/qcsystem/extension/portlet/WorkflowTransformRest.java
@@ -0,0 +1,41 @@
+package com.jb4dc.qcsystem.webpackage.rest.qcsystem.extension.portlet;
+
+import com.github.pagehelper.PageInfo;
+import com.jb4dc.base.service.general.JB4DCSessionUtility;
+import com.jb4dc.base.ymls.JBuild4DCYaml;
+import com.jb4dc.core.base.exception.JBuild4DCGenerallyException;
+import com.jb4dc.core.base.vo.JBuild4DCResponseVo;
+import com.jb4dc.workflow.client.rest.InstanceRuntimeRest;
+import com.jb4dc.workflow.exenum.WorkFlowEnum;
+import com.jb4dc.workflow.po.ExecutionTaskPO;
+import com.jb4dc.workflow.searchmodel.ExecutionTaskSearchModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/Rest/Extension/Portlet/WorkflowTransform")
+public class WorkflowTransformRest {
+
+    @Autowired
+    InstanceRuntimeRest instanceRuntimeRest;
+
+    @RequestMapping(value = "/GetMyProcessTaskList", method = {RequestMethod.GET, RequestMethod.POST})
+    public JBuild4DCResponseVo<List<ExecutionTaskPO>> getMyProcessTaskListTransform(String modelCategory,int pageSize) throws JBuild4DCGenerallyException {
+        ExecutionTaskSearchModel executionTaskSearchModel=new ExecutionTaskSearchModel();
+        executionTaskSearchModel.setModelCategory(modelCategory);
+        executionTaskSearchModel.setExtaskType(WorkFlowEnum.ExTask_Type_Main);
+        executionTaskSearchModel.setLinkId(JBuild4DCYaml.getLinkId());
+        executionTaskSearchModel.setJb4DCSession(JB4DCSessionUtility.getSession());
+        executionTaskSearchModel.setPageNum(1);
+        executionTaskSearchModel.setPageSize(pageSize);
+        executionTaskSearchModel.setLoadDict(false);
+
+        List<ExecutionTaskPO> executionTaskPOList=instanceRuntimeRest.getMyProcessTaskList(executionTaskSearchModel).getData().getList();
+        return JBuild4DCResponseVo.getDataSuccess(executionTaskPOList);
+    }
+
+}
